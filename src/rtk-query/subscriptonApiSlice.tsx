c543c818892eee0cb5a07dd87b22f8b1bca0b9d3
keshav-groyyo <keshav@groyyo.com>
2024-10-28 14:03:43 +0530

    added

--- src/rtk-query/subscriptonApiSlice.tsx
+++ src/rtk-query/subscriptonApiSlice.tsx
@@ -1,19 +1,36 @@
 import apiSlice from "./apiSlice";
 
-const subscriptionUrl = "subscription"
+const subscriptionUrl = "subscription";
 
-const subscriptionApiSlice =  apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        createSubscription: builder.mutation({
-            query: ({payload, token}: {payload: any, token: string}) => ({
-                url: `${subscriptionUrl}/createSubscription`,
-                method: "POST",
-                body: payload
-            })
-        })
-    })
+const subscriptionApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createSubscription: builder.mutation({
+      query: ({ payload, token }: { payload: any, token: string }) => ({
+        url: `${subscriptionUrl}/createSubscription`,
+        method: "POST",
+        body: payload,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }),
+    }),
+    getSubscriptionList: builder.query({
+      query: () => ({
+        url: `${subscriptionUrl}/list`,
+        method: "GET",
+      }),
+    }),
+    getSubscriptionById: builder.query({
+      query: (id: number) => ({
+        url: `${subscriptionUrl}/byId/${id}`,
+        method: "GET",
+      }),
+    }),
+  }),
 });
 
 export const {
-    useCreateSubscriptionMutation
-} = subscriptionApiSlice
+  useCreateSubscriptionMutation,
+  useGetSubscriptionListQuery,
+  useGetSubscriptionByIdQuery,
+} = subscriptionApiSlice;
