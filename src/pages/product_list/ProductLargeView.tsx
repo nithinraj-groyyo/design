933011b8c29c5bc42bb48b79f37fc813c91118de
nithinraj-groyyo <nithinraj@groyyo.com>
2024-10-28 12:42:11 +0530

    fix: product large view

--- src/pages/product_list/ProductLargeView.tsx
+++ src/pages/product_list/ProductLargeView.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { IProduct } from '../../types/products';
+import { useNavigate } from 'react-router-dom';
 
 interface IProductLargeViewProps {
   products: IProduct[];
 }
 
 const ProductLargeView = ({ products }: IProductLargeViewProps) => {
+  const navigate = useNavigate();
+
+  const navigateToProductDetails = (productId: number) => {
+    navigate(`/product-details/${productId}`);
+  };
+
   return (
     <>
-      <div className="xxs:hidden lg:flex flex-col w-full mt-[5rem] lg:mt-[12rem]">
+      {/* <div className="xxs:hidden lg:flex flex-col w-full mt-[5rem] lg:mt-[12rem]">
         <div className="flex flex-row">
           <div className="flex-1 h-[88.188rem]">
             <img className="h-full w-full" src={"/images/products/pic1.png"} alt="Image 1" />
@@ -30,12 +37,35 @@ const ProductLargeView = ({ products }: IProductLargeViewProps) => {
         <div className="h-[164.5rem]">
           <img className="h-full w-full" src={"/images/products/pic5.png"} alt="Image 5" />
         </div>
+      </div> */}
+      <div className="xxs:hidden lg:grid grid-cols-2 gap-4 mt-[10rem]">
+        {products && products.map((product, index) => (
+          <div
+            key={product?.id}
+            className={`${index === products.length - 1 && products.length % 2 !== 0
+                ? 'col-span-2'
+                : ''
+              }`}
+          >
+            <img
+              src={(product?.productImages.find(image => image.isThumbnail === true))?.signedUrl}
+              alt={product?.name}
+              className="w-full h-full object-cover cursor-pointer"
+              onClick={() => navigateToProductDetails(product?.id)}
+            />
+          </div>
+        ))}
       </div>
       <div className="lg:hidden mt-[5rem] lg:mt-[12rem]">
         {products && products?.map((product) => {
           return (
             <div className="flex-1" key={product?.id}>
-              <img className="h-full w-full" src={(product?.productImages.find(image => image.isThumbnail === true))?.signedUrl} alt="Image 1" />
+              <img 
+                className="h-full w-full cursor-pointer" 
+                src={(product?.productImages.find(image => image.isThumbnail === true))?.signedUrl} 
+                alt={product?.name} 
+                onClick={() => navigateToProductDetails(product?.id)}
+              />
             </div>
           )
         })}
