16bf52570c20ac94d9d04370c265faa91358308a
tirath2 <btirath2@gmail.com>
2025-04-09 12:29:58 +0530

    intial  fix

--- src/pages/productDetails/ImageSlider.tsx
+++ src/pages/productDetails/ImageSlider.tsx
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
-import Slider from "react-slick";
+import React, { useRef, useEffect, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { IProduct } from "../../types/products";
-
+import _Slider from "react-slick";
+const Slider: any = _Slider;
 interface IImageSliderProps {
   product: IProduct;
 }
 
 const ImageSlider = ({ product }: IImageSliderProps) => {
-  const [nav1, setNav1] = useState<Slider | null>(null);
-  const [nav2, setNav2] = useState<Slider | null>(null);
+  const slider1 = useRef<_Slider>(null);
+  const slider2 = useRef<_Slider>(null);
+  const [nav1, setNav1] = useState<_Slider | null>(null);
+  const [nav2, setNav2] = useState<_Slider | null>(null);
+
+  useEffect(() => {
+    setNav1(slider1.current);
+    setNav2(slider2.current);
+  }, []);
 
   return (
     <div className="flex-1">
@@ -19,15 +26,21 @@ const ImageSlider = ({ product }: IImageSliderProps) => {
         <div className="flex-shrink-0 w-[80%]">
           <Slider
             asNavFor={nav2 ?? undefined}
-            ref={(slider1) => setNav1(slider1)}
+            ref={slider1}
             slidesToShow={1}
             slidesToScroll={1}
             arrows={false}
             vertical={true}
+            dots={true}
+            infinite={true}
+            speed={500}
           >
             {product?.productImages?.map((img, index) => (
               <div key={index} className="w-full">
-                <div className="relative" style={{ paddingBottom: '133.33%' /* 12:9 aspect ratio */ }}>
+                <div
+                  className="relative"
+                  style={{ paddingBottom: "133.33%" /* 12:9 aspect ratio */ }}
+                >
                   <img
                     src={img?.signedUrl}
                     alt={`Product ${index}`}
@@ -43,7 +56,7 @@ const ImageSlider = ({ product }: IImageSliderProps) => {
         <div className="w-[20%]">
           <Slider
             asNavFor={nav1 ?? undefined}
-            ref={(slider2) => setNav2(slider2)}
+            ref={slider2}
             swipeToSlide={true}
             slidesToShow={4}
             slidesToScroll={1}
@@ -54,7 +67,10 @@ const ImageSlider = ({ product }: IImageSliderProps) => {
           >
             {product?.productImages?.map((img, index) => (
               <div key={index} className="w-full">
-                <div className="relative" style={{ paddingBottom: '111%' /* 12:9 aspect ratio */ }}>
+                <div
+                  className="relative"
+                  style={{ paddingBottom: "111%" /* 12:9 aspect ratio */ }}
+                >
                   <img
                     src={img?.signedUrl}
                     alt={`Thumbnail ${index}`}
